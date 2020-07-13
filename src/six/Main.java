4ba6fbb52a7fb4ba85e75e8e7557fb4fd209c760
tako_eng <oys.sys@gmail.com>
2020-07-14 08:10:10 +0900

    add account

---
+++ src/six/Main.java
@@ -0,0 +1,19 @@
+package six;
+
+interface Func1{
+	boolean call(int x);
+}
+
+interface Func2{
+	String call(boolean male, String name);
+}
+
+public class Main {
+	public static void main(String args[]) {
+		FuncList funcList = new FuncList();
+		Func1 f1 = FuncList::isOdd;
+		Func2 f2 = funcList::addNamePrefix;
+		System.out.println(f1.call(15));
+		System.out.println(f2.call(true, "Smith"));
+	}
+}
